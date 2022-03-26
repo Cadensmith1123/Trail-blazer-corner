ccc2b62e7ab16f5f1f47a3c51547211cae2d462b
Caden Smith <cadensmith1123@gmail.com>
2022-03-25 23:12:18 -0700

    Took Loader out

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -2,7 +2,8 @@ import React from 'react'
 import { useRouter } from 'next/router'
 
 import { getPosts, getPostDetails } from '../../services'
-import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components'
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components'
+
 
 const PostDetails = ({ post }) => {
 const router = useRouter();
